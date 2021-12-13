f8a29ee37aa5a38bfa0556aa1addf4492ccce405
Ryan Wang <i@ryanc.cc>
2021-12-13 16:16:01 +0800

    refactor: website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: `/`, // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: `Ryan Wang's Running Page`,
+    siteUrl: 'https://running.ryanwang.me',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://ryanc.cc',
       },
       {
         name: 'About',
